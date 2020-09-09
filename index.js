f93cd35ad0afb1e591484164854383433104704a
Tim Eichinger <privat.timeichinger@gmail.com>
2020-09-09 22:30:58 +0200

    adjusted server code for azure-deployment

--- index.js
+++ index.js
@@ -3,16 +3,15 @@
 const AppServer = require("./lib/AppServer.js");
 
 var server;
+var port = process.env.PORT || 8000;
 
 /**
  * Starts webserver to serve files from "/app" folder
  */
 function init() {
-  // Access command line parameters from start command (see package.json)
-  let appDirectory = process.argv[2], // folder with client files
-    appPort = process.argv[3]; // port to use for serving static files
-  server = new AppServer(appDirectory);
-  server.start(appPort);
+  
+  server = new AppServer();
+  server.start(port);
 }
 
 init();
