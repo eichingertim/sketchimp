4b87bbcc28f2bd29d831f0f58175d85cf44e8742
tobiasdollhofer <tdollhofer96@googlemail.com>
2020-09-14 17:51:10 +0200

    added hardcoded appdirectory and port to index.js

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ var server;
  */
 function init() {
   // Access command line parameters from start command (see package.json)
-  let appDirectory = process.argv[2], // folder with client files
-    appPort = process.argv[3]; // port to use for serving static files
+  /*let appDirectory = process.argv[2], // folder with client files
+    appPort = process.argv[3]; */// port to use for serving static files
+    let appDirectory = "./app",
+    appPort = 8000;
   server = new AppServer(appDirectory);
   server.start(appPort);
 }
