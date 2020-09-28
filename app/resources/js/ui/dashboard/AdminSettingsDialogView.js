c9eb1ec6fd68e8815e7cb084c3465e4b588ce0cb
Timon Lorenz <TimmensOne@users.noreply.github.com>
2020-09-28 17:14:59 +0200

    add role-tag color

--- app/resources/js/ui/dashboard/AdminSettingsDialogView.js
+++ app/resources/js/ui/dashboard/AdminSettingsDialogView.js
@@ -1,10 +1,10 @@
 import View from "../View.js";
 import { Event } from "../../utils/Observable.js";
-import {EventKeys} from "../../utils/Config.js";
+import { EventKeys } from "../../utils/Config.js";
 
 class SaveSettingsClickEvent extends Event {
     constructor(id) {
-        super(EventKeys.SAVE_SETTINGS_CLICK, {id: id});
+        super(EventKeys.SAVE_SETTINGS_CLICK, { id: id });
     }
 }
 
@@ -17,31 +17,44 @@ function onSaveSettingsClick(adminSettingsDialogView, data) {
 function setListener(adminSettingsDialogView) {
     adminSettingsDialogView.el.querySelector(".save-settings")
         .addEventListener("click", onSaveSettingsClick.bind(this, adminSettingsDialogView));
-    //
+
     Array.from(adminSettingsDialogView.el.querySelectorAll(".member-item")).forEach(member => {
         member.querySelector(".admin").addEventListener("click", () => {
-            member.querySelector(".role-tag").textContent = "admin";
+            let element = member.querySelector(".role-tag");
+            element.textContent = "Admin";
+            setLabelColor(element, "Admin");
+            //element.classList.remove("label-info","label-danger");
+            //element.classList.add("label-success");
         });
         member.querySelector(".collaborator").addEventListener("click", () => {
-            member.querySelector(".role-tag").textContent = "collaborator";
+            let element = member.querySelector(".role-tag");
+            element.textContent = "Collaborator";
+            setLabelColor(element, "Collaborator");
+            //element.classList.remove("label-success", "label-success");
+            //element.classList.add("label-info");
         });
         member.querySelector(".observer").addEventListener("click", () => {
-            member.querySelector(".role-tag").textContent = "observer";
+            let element = member.querySelector(".role-tag");
+            element.textContent = "Observer";
+            setLabelColor(element, "Observer");
+            //element.classList.remove("label-success", "label-info");
+            //element.classList.add("label-danger");
         });
     });
-    // adminSettingsDialogView.el.querySelector(".admin")
-    //     .addEventListener("click", () => {
-    //         adminSettingsDialogView.el.querySelector(".role-tag").textContent = "admin";
-    //     });
-    // adminSettingsDialogView.el.querySelector(".collaborator")
-    //     .addEventListener("click", () => {
-    //         adminSettingsDialogView.el.querySelector(".role-tag").textContent = "collaborator";
-    //     });
-    // adminSettingsDialogView.el.querySelector(".observer")
-    //     .addEventListener("click", () => {
-    //         adminSettingsDialogView.el.querySelector(".role-tag").textContent = "observer";
-    //     });
+}
+
+function setLabelColor (element, role) {
+    if(role === "Admin"){
+        element.classList.remove("label-info","label-danger");
+        element.classList.add("label-success");
+    } else if(role === "Collaborator"){
+        element.classList.remove("label-success", "label-success");
+        element.classList.add("label-info");
+    } else {
+        element.classList.remove("label-success", "label-info");
+        element.classList.add("label-danger");
     }
+}
 
 class AdminSettingsDialogView extends View {
     constructor(el) {
@@ -54,15 +67,15 @@ class AdminSettingsDialogView extends View {
         this.el.querySelector(".form-control").value = channelName;
         Array.from(this.el.querySelectorAll(".member-item")).forEach((member, index) => {
             member.querySelector(".role-tag").textContent = users[index].role;
+            setLabelColor(member.querySelector(".role-tag"), users[index].role);
         });
-        //this.el.querySelector(".role-tag").textContent = users;
     }
 
     setSettings(channel) {
         channelId = channel.channelId;
         channelName = channel.channelName;
         users = channel.members.map(member => {
-            return {id: member.id, name: member.username, role: member.role};
+            return { id: member.id, name: member.username, role: member.role };
         });
     }
 
