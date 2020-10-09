12bba0fe6329b7b7cd1e39a0360c55002943ed75
luggaesch <lukas.schauhuber1@gmail.com>
2020-10-09 14:31:11 +0200

    fixed some minor issues after lint cleanup

--- app/resources/js/profile.js
+++ app/resources/js/profile.js
@@ -1,8 +1,22 @@
-document.querySelector(".profile-image-overlay").addEventListener("click", (event) => {
-    document.querySelector("#profile-upload").click();
-});
+let profileImageOverlay,
+    profileUpload,
+    selectedFile,
+    uploadButtton;
 
-document.querySelector("#profile-upload").addEventListener("change", (event) => {
-    document.querySelector("#selected-file").innerHTML = event.target.value;
-    document.querySelector("#btn-upload-profile").style.visibility = "visible";
-});
+function init() {
+    profileImageOverlay = document.querySelector(".profile-image-overlay");
+    profileUpload = document.querySelector("#profile-upload");
+    selectedFile = document.querySelector("#selected-file");
+    uploadButtton = document.querySelector("#btn-upload-profile");
+
+    profileImageOverlay.addEventListener("click", () => {
+        profileUpload.click();
+    });
+
+    profileUpload.addEventListener("change", (event) => {
+        selectedFile.innerHTML = event.target.value;
+        uploadButtton.style.visibility = "visible";
+    });
+}
+
+init();
