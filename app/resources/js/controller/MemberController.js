bef7b7f7d8b82128246807a0499459e5b6058419
Tim Eichinger <privat.timeichinger@gmail.com>
2020-10-09 17:27:00 +0200

    commented frontend controllers

--- app/resources/js/controller/MemberController.js
+++ app/resources/js/controller/MemberController.js
@@ -1,8 +1,16 @@
 import {Config} from "../utils/Config.js";
 import Helper from "../utils/Helper.js";
 
+/**
+ * Class with static Methods to handle AJAX and therefore all XMLHttpRequests belonging to users/members
+ */
 class MemberController {
 
+    /**
+     * fetches data of members that was hovered over in the dashboard
+     * @param url api-url with the memberId to call for
+     * @returns {Promise<>} wait for request to finish
+     */
     static fetchMemberData(url) {
         return new Promise(
             function (resolve, reject) {
